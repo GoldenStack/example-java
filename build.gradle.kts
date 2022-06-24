3b9399ed4d0eb87860bc163b699cfb3f2496912c
GoldenStack <goldenfire64yt@gmail.com>
2022-06-24 00:33:43 -0500

    Add more information to build.gradle.kts
    Signed-off-by: GoldenStack <goldenfire64yt@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,28 @@
 plugins {
-    id("java")
+    `java-library`
+    `maven-publish`
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
+description = "Example project"
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("org.jetbrains:annotations:23.0.0")
+
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
